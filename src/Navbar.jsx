70a3c1811e1670f7d6e07df89c47012892c4cac6
brendanobilo <brendanxure@outlook.com>
2025-09-12 15:52:34 -0400

    update

--- src/Navbar.jsx
+++ src/Navbar.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 const Navbar = () => {
     return (
@@ -11,8 +12,8 @@ const Navbar = () => {
                     </button>
                     <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
                         <div class="navbar-nav">
-                            <a class="nav-link active" aria-current="page" href="/">Home</a>
-                            <a class="nav-link" href="/add">Add Products</a>
+                            <Link to="/" class="nav-link active" aria-current="page"> Home</Link>
+                            <Link to="/add" class="nav-link">Add Products</Link>
                         </div>
                     </div>
                 </div>
