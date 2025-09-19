50f0a7b81199fbfa4f364b5019ffc4f59abada78
brendanobilo <brendanxure@outlook.com>
2025-09-19 08:12:20 -0400

    update

--- src/Footer.jsx
+++ src/Footer.jsx
@@ -1,8 +1,10 @@
 import React from 'react'
 
 const Footer = () => {
+    const date = new Date();
+    const year = date.getFullYear();
     return (
-        <div><footer class="footer mt-auto py-3 bg-body-tertiary"> Xurepify</footer></div>
+        <div><footer class="footer mt-auto py-3 bg-body-tertiary"> &copy; ${year} Xurepify</footer></div>
     )
 }
 
