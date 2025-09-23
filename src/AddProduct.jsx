5cb8770eff24748f7afe8bf82df5ab4b3fcb3515
brendanobilo <brendanxure@outlook.com>
2025-09-22 20:25:23 -0400

    update

--- src/AddProduct.jsx
+++ src/AddProduct.jsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from 'react'
-import { data, useNavigate } from 'react-router-dom';
+import { useNavigate } from 'react-router-dom';
 
 const AddProduct = () => {
     const [formData, setFormData] = useState({
