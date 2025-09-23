5a57f2661b514880729d390df7688f5f7826cbda
brendanobilo <brendanxure@outlook.com>
2025-09-23 19:35:10 -0400

    update

--- src/Navbar.jsx
+++ src/Navbar.jsx
@@ -3,7 +3,7 @@ import { Link } from 'react-router-dom'
 
 const Navbar = () => {
     return (
-        <div>
+        <div class='container-fluid'>
             <nav class="navbar navbar-expand-lg bg-body-tertiary">
                 <div class="container-fluid">
                     <a class="navbar-brand" href="#">Xurepify</a>
