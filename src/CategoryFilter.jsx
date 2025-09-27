049207f9f66ed716b1c50aac4c8e30838b50092e
brendanobilo <brendanxure@outlook.com>
2025-09-27 19:42:03 -0400

    update

--- src/CategoryFilter.jsx
+++ src/CategoryFilter.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const CategoryFilter = ({ category, onSelect }) => {
     return (
         <div>
-            <select name="" id="categorySelect" className='form-control' onChange={(e) => onSelect(e.target.value)}>
+            <select name="categorySelect" id="categorySelect" className='form-control' onChange={(e) => onSelect(e.target.value)}>
                 <option value="">All Categories</option>
                 {category.map(eachCat =>
                     <option key={eachCat.id} value={eachCat.id}>
